98b95ad7ae13a08b88c8429ec18db234dd0ad9c9
Muhammad Abdur Rehman Siddiqi <164844124+manisiddiqi@users.noreply.github.com>
2026-03-06 08:36:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const mode = document.getElementById("mode");
+const heading = document.getElementById("heading");
+
+mode.addEventListener("click", ()=> {
+    if(document.body.style.backgroundColor == "white"){
+        document.body.style.backgroundColor = "black";
+        heading.textContent = "Dark Mode";
+        heading.style.color = "white";
+        document.title = "Dark Mode";
+        mode.textContent = "🌛"
+    }else{
+        document.body.style.backgroundColor = "white";
+        heading.textContent = "Light Mode";
+        heading.style.color = "black";
+        document.title = "Light Mode";
+        mode.textContent = "🌞"
+    }
+})
